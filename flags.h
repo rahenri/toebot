fabd289541e9f8863683f61844106b697e93ad21
Raphael H Ribas <raphael.ribas@gmail.com>
2016-04-24 22:13:34 -0700

    fix flags

--- flags.h
+++ flags.h
@@ -1,10 +1,10 @@
 #ifndef FLAGS_H
 #define FLAGS_H
 
-static const bool PlayDeterministic = true;
+static const bool PlayDeterministic = false;
 static const bool PrintSearchTree = false;
 static const bool PonderMode = false;
 static const int DefaultTimeLimit = 800;
-static const int MaxDepth = 12;
+static const int MaxDepth = 50;
 
 #endif
