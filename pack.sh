9366c1c7a7831f081bccb422d0bd98d3529b7b57
Raphael H Ribas <raphael.ribas@gmail.com>
2016-12-25 02:13:18 -0800

    Really fix pack script

--- pack.sh
+++ pack.sh
@@ -4,6 +4,6 @@ output=toebot.tar.gz
 
 rm -rf ${output}
 
-FILES=$(find .  | grep '\.\(h\|cpp\)$' | grep -v '_test\/cpp$')
+FILES=$(find .  | grep '\.\(h\|cpp\)$' | grep -v '_test\.cpp$')
 
 tar -czf ${output} ${FILES}
